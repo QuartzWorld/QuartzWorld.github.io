83fe8751b3836a108a1bdbd1d656109aa7b1b591
Choi Yoons <ys.choi@me.com>
2018-04-01 11:39:40 +0900

    all new

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
 import { browser, by, element } from 'protractor';
 
-export class WwwPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MdToolbarModule, MdButtonModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ConfirmValidatorDirective } from './confirm-validator.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    ConfirmValidatorDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    // Material
-    MdToolbarModule,
-    MdButtonModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
